96c63c842fba5a30d4daa86ffa18962df8eb6947
leucht <lucas.philipp@st.ovgu.de>
2022-12-05 23:33:53 +0100

    Add homescreen logic

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,10 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import '../providers/theme_provider.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends ConsumerStatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
+  ConsumerState<HomeScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends ConsumerState<HomeScreen> {
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Padding(
+          padding: const EdgeInsets.all(10),
+          child: ListView(
+            children: <Widget>[
+              Container(
+                  alignment: Alignment.topLeft,
+                  padding: const EdgeInsets.all(10),
+                  child: const Text(
+                    'SimpleApp',
+                    style: TextStyle(
+                        color: Colors.purple,
+                        fontWeight: FontWeight.w500,
+                        fontSize: 30),
+                  )),
+              Container(
+                alignment: Alignment.topRight,
+                padding: const EdgeInsets.all(10),
+                child: IconButton(
+                  onPressed: () {
+                    ref.read(themeProvider.notifier).toggleTheme();
+                  },
+                  icon: const Icon(
+                    Icons.add_circle,
+                  ),
+                ),
+              ),
+            ],
+          )),
+    );
   }
 }
